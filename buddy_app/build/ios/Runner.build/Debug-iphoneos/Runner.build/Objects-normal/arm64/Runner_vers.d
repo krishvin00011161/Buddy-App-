876bd959d9f87cdd48ea77be4ea5c326de81a832
David Kim <davidkim120302@gmail.com>
2020-04-08 15:59:49 -0400

    Firebase
    Login and chatting ability had been completed

---
+++ buddy_app/build/ios/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/david/Desktop/Buddy-App-/buddy_app/build/ios/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
---
+++ buddy_app/build/ios/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/GeneratedPluginRegistrant.d
@@ -0,0 +1,19 @@
+dependencies: \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Modules/module.modulemap \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/david/Desktop/Buddy-App-/buddy_app/ios/Flutter/Flutter.framework/Headers/Flutter.h
