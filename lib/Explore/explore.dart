e8ff6a0f7195422d7f497701c72d56924d3508f1
David Kim <davidkim120302@gmail.com>
2020-06-12 10:50:31 -0400

    Settings and Explore
    UI switch
    - Settings
    - explore
    - may be bugs due to not testing out

--- lib/Explore/explore.dart
+++ lib/Explore/explore.dart
@@ -1,6 +1,5 @@
-import 'package:buddyappfirebase/home/widgets/custom_app_bar.dart';
-import 'package:buddyappfirebase/home/widgets/custom_drawers.dart';
 import 'package:flutter/material.dart';
+
 
 class ExplorePage extends StatefulWidget {
   @override
@@ -11,8 +10,49 @@ class _ExplorePageState extends State<ExplorePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(),
-      drawer: CustomDrawers(),
-    );
+      appBar: AppBar(
+        title: Text("Explore page"),
+      ),
+      body: ListView(
+
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(11.5),
+            child: TextField(
+              onChanged: (val){
+
+                //findQuestion(val);
+              },
+              decoration: InputDecoration(
+                prefix:IconButton(
+                  color:Colors.black,
+                  icon:Icon(Icons.arrow_back),
+                  iconSize: 20.0,
+                  onPressed: (){
+                    Navigator.of(context).pop();
+                  },
+
+
+                ),
+                contentPadding: EdgeInsets.only(left: 26.5),
+                hintText:'Search Question',
+                border:OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(4.0))),
+                ),
+              ),
+              ],
+            ),
+          );
   }
 }
+// class ExplorePage extends StatefulWidget {
+//   @override
+//   _ExplorePageState createState() => _ExplorePageState();
+// }
+
+// class _ExplorePageState extends State<ExplorePage> {
+//   @override
+//   Widget build(BuildContext context) {
+    
+//   }
+// }
--- lib/Settings/settings.dart
+++ lib/Settings/settings.dart
@@ -1,7 +1,6 @@
-import 'package:buddyappfirebase/home/widgets/custom_app_bar.dart';
-import 'package:buddyappfirebase/home/widgets/custom_drawers.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
-
+import 'package:link/link.dart';
 class SettingsView extends StatefulWidget {
   SettingsView({Key key}) : super(key: key);
 
@@ -13,8 +12,49 @@ class _SettingsViewState extends State<SettingsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(),
-      drawer: CustomDrawers(),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("Settings"),
+      ),
+      body: Column(
+        children: <Widget>[
+          SwitchListTile(
+            value: false,
+            activeColor: Colors.green[500],
+            title: Text("Notifications"),
+            onChanged:(val){},
+          ),
+          SwitchListTile(
+            value: false,
+            activeColor: Colors.green[500],
+            title: Text("Dark Mode"),
+            onChanged:(bool value){
+              setState(() {
+//                var settingsDarkViewState = _SettingsDarkViewState();
+
+              });
+            },
+          ),
+          ExpansionTile(
+            title: Text("Private Policy Link Below"),
+            children: <Widget>[
+              Link(
+                child: Text("Private Policy"),
+                url:'https://docs.google.com/document/d/1TAqTE7MBzuIagISHHzjGxSHoY1z884LXR3iGIojz1sA/edit',
+              )
+            ],
+          ),
+
+
+
+        ],
+      ),
+
+
+
+
     );
+
+
   }
 }
