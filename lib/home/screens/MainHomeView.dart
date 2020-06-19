804030b802cbb6b458bf1e0cfc2366f283363500
David Kim <davidkim120302@gmail.com>
2020-06-19 10:02:25 -0400

    Home UI
    Home
    UI

--- lib/home/screens/MainHomeView.dart
+++ lib/home/screens/MainHomeView.dart
@@ -2,12 +2,13 @@ import 'package:buddyappfirebase/home/animation/FadeAnimation.dart';
 import 'package:buddyappfirebase/login/constants/route_names.dart';
 import 'package:buddyappfirebase/login/services/navigation_service.dart';
 import 'package:buddyappfirebase/login/locator.dart';
-import 'package:buddyappfirebase/home/widgets/custom_app_bar.dart';
 import 'package:buddyappfirebase/home/widgets/custom_drawers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
+//Dk
 
 class MainHomeView extends StatefulWidget {
   @override
@@ -17,20 +18,26 @@ class MainHomeView extends StatefulWidget {
 class _MainHomeViewState extends State<MainHomeView> {
   final GoogleSignIn googleSignIn = new GoogleSignIn();
   final NavigationService _navigationService = locator<NavigationService>();
-
-
-
-
+  
 
   logout() {
     googleSignIn.signOut();
     _navigationService.navigateTo(StartViewRoute);
   }
 
-  Scaffold home() {
+
+
+  Scaffold home() { // This is responsible for the Home View
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      appBar: AppBar(
+      appBar: homeAppBar(),
+      drawer: CustomDrawers(),
+      body: homeBody(),
+    );
+  }
+
+  AppBar homeAppBar() { // App bar
+    return AppBar(
         iconTheme: new IconThemeData(color: Colors.grey),
         backgroundColor: Colors.grey[100],
         elevation: 0,
@@ -43,17 +50,17 @@ class _MainHomeViewState extends State<MainHomeView> {
               border: new OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(10)),
                       borderSide: new BorderSide(color: Colors.grey, width: 3)),
-              //prefixIcon: Icon(Icons.search, color: Colors.grey,),
-              //icon: Icon(Icons.search, color: Colors.grey,),
               hintStyle: TextStyle(color: Colors.grey, fontSize: 15, fontWeight: FontWeight.bold),
               hintText: "Ask a question", 
               suffixIcon: Icon(Icons.search)
             ),
           ),
         ),
-      ),
-      drawer: CustomDrawers(),
-      body: SingleChildScrollView(
+      );
+  }
+
+  SingleChildScrollView homeBody() { // Anything body related
+    return SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
@@ -87,15 +94,36 @@ class _MainHomeViewState extends State<MainHomeView> {
                     child: ListView(
                       scrollDirection: Axis.horizontal,
                       children: <Widget>[
-                        Groups(image: 'assets/images/0.jpg', title: 'U.S. History'),
-                        Groups(image: 'assets/images/0.jpg', title: 'Chemistry'),
-                        Groups(image: 'assets/images/2.jpg', title: 'Greece'),
-                        Groups(image: 'assets/images/7.jpg', title: 'United States')
+                        groups(title: 'U.S. History'),
+                        groups(title: 'Chemistry'),
+                        groups(title: 'Greece'),
+                       
                       ],
                     ),
                   )),
-
-                  SizedBox(height: 60,),
+                  SizedBox(height: 30,),
+                  Row(children: [
+                    SizedBox(width: 60,),
+                    Container(
+                      height: 8,
+                      width: 70,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(width: 30,),
+                    Container(
+                      height: 8,
+                      width: 70,
+                      color: Colors.grey[300],
+                    ),
+                    SizedBox(width: 30,),
+                    Container(
+                      height: 8,
+                      width: 70,
+                      color: Colors.grey[300],
+                    ),
+                  ],
+                  ),
+                  SizedBox(height: 30,),
                   Row(
                     children: <Widget>[
                       FadeAnimation(1, Text("Questions", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 35),)),
@@ -112,10 +140,10 @@ class _MainHomeViewState extends State<MainHomeView> {
                     child: ListView(
                       scrollDirection: Axis.horizontal,
                       children: <Widget>[
-                        Questions(image: 'assets/images/5.jpg', title: 'Canada'),
-                        Questions(image: 'assets/images/6.jpg', title: 'Italy'),
-                        Questions(image: 'assets/images/7.jpg', title: 'Greece'),
-                        Questions(image: 'assets/images/8.jpg', title: 'United States')
+                        questions(title: 'Canada'),
+                        questions(title: 'Italy'),
+                        questions(title: 'Greece'),
+                        questions(title: 'United States')
                       ],
                     ),
                   )),
@@ -125,21 +153,23 @@ class _MainHomeViewState extends State<MainHomeView> {
             )
           ],
         ),
-      ),
-    );
+      );
   }
 
   @override
   Widget build(BuildContext context) {
+    
     return home();
   }
 }
 
 
-  Widget Groups({image, title}) {
+  Widget groups({title}) {  // Makes Rectangles belongs in the groups section
     return AspectRatio(
       aspectRatio: 10/9.3,
       child: Container( 
+        width: 100,
+        padding: EdgeInsets.all(4.0),
         margin: EdgeInsets.only(right: 15),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
@@ -156,7 +186,7 @@ class _MainHomeViewState extends State<MainHomeView> {
               blurRadius: 7,
               offset: Offset(0, 3), // changes position of shadow
             ),
-    ],
+            ],
             gradient: LinearGradient(
               begin: Alignment.bottomRight,
               colors: [
@@ -165,44 +195,178 @@ class _MainHomeViewState extends State<MainHomeView> {
               ]
             )
           ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
+          child: Column(
             children: [
               Align(
               alignment: Alignment.topLeft,
-              child: Text(title, style: GoogleFonts.roboto(textStyle: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold)),),
+              child: Flexible(child: Text(title, style: GoogleFonts.roboto(textStyle: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold)), textAlign: TextAlign.left,)),
               ),
+              SizedBox(height: 10,),
               Container(
-                alignment: Alignment.topLeft,
-                color: Colors.blue,
-                    height: 40,
-                    width: 40,
+                width: 250.0,
+                height: 170.0,
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                boxShadow: [
+                  BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: -10,
+                  blurRadius: 0,
+                  offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+                color: Colors.transparent,
+                ),
+                child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 5.0),
+        padding: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(9.0),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey[300], blurRadius: 5.0, offset: Offset(0, 3))
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage("https://picturecorrect-wpengine.netdna-ssl.com/wp-content/uploads/2014/03/portrait-photography.jpg"),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Yo, Send me the Answers to the HW",
+                        style: TextStyle(fontSize: 16),
+                        maxLines: 3,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 10),
+            Column(
+              children: [
+                Row(
+                children: [
+                CircleAvatar(
+                  radius: 12,
+                  backgroundImage: NetworkImage(""),
+                  backgroundColor: Colors.blue,
+                ),
+                SizedBox(width: 7),
+                CircleAvatar(
+                  radius: 12,
+                  backgroundImage: NetworkImage(""),
+                  backgroundColor: Colors.blue,
+                ),
+                SizedBox(width: 7),
+                CircleAvatar(
+                  radius: 12,
+                  backgroundImage: NetworkImage(""),
+                  backgroundColor: Colors.blue,
+                ),
+                SizedBox(width: 7),
+                CircleAvatar(
+                  radius: 12,
+                  backgroundImage: NetworkImage(""),
+                  backgroundColor: Colors.blue,
+                ),
+                SizedBox(width: 7),
+                CircleAvatar(
+                  radius: 12,
+                  backgroundImage: NetworkImage(""),
+                  backgroundColor: Colors.blue,
+                ),
+                SizedBox(width: 7),
+              ],
+            ),
+            SizedBox(height: 10,),
+              Row(
+                children: [
+                CircleAvatar(
+                  radius: 8,
+                  backgroundColor: Colors.blue,
+                ),
+                SizedBox(width: 7),
+                CircleAvatar(
+                  radius: 8,
+                  backgroundColor: Colors.blue,
+                ),
+                SizedBox(width: 7),
+                CircleAvatar(
+                  radius: 8,
+                  backgroundColor: Colors.blue,
+                ),
+                SizedBox(width: 7),
+                CircleAvatar(
+                  radius: 8,
+                  backgroundColor: Colors.blue,
+                ),
+                SizedBox(width: 30),
+                Container(
+                height: 20.0,
+                width: 80,
+                color: Colors.transparent,
+                child: new Container(
+                decoration: new BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: new BorderRadius.only(
+                topLeft: const Radius.circular(10.0),
+                topRight: const Radius.circular(10.0),
+                bottomLeft: const Radius.circular(10.0),
+                bottomRight: const Radius.circular(10.0),
+              )
+            ),
+            child: new Center(
+            child: new Text("11:26 pm"),
+           )
+         ),
+         ),
+              ],
+            ),
+              ],
+            ),
+          ],
+        ),
+      ),
               ),
             ],
           ),
-          // child: Align(
-          //   alignment: Alignment.topLeft,
-          //   child: Text(title, style: GoogleFonts.roboto(textStyle: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold)),),
-          // ),
-          
         ), // this is responsible for the inside
       ), // overall container
     );
   }
 
-  Widget Questions({image, title}) {
+  Widget questions({title}) { // Makes rectangles belongs in the questions section 
     return AspectRatio(
+      
       aspectRatio: 4 / 3,
       child:  Container(
         margin: EdgeInsets.only(right: 15),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
+          borderRadius: BorderRadius.circular(10),
           color: Color(0xF1C40F),
         ),
         child: Container(
           padding: EdgeInsets.all(20),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(25),
+            boxShadow: [
+              BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 4,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+            ],
             gradient: LinearGradient(
               begin: Alignment.bottomRight,
               colors: [
@@ -214,20 +378,86 @@ class _MainHomeViewState extends State<MainHomeView> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-             
-
-
-
-              // Align(
-              // alignment: Alignment.topLeft,
-              // child: Text(title, style: GoogleFonts.roboto(textStyle: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold)),),
-              // ),
-              // Container(
-              //   alignment: Alignment.topLeft,
-              //   color: Colors.blue,
-              //       height: 40,
-              //       width: 40,
-              // ),
+              Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    CircleAvatar(
+                    backgroundImage: NetworkImage("https://picturecorrect-wpengine.netdna-ssl.com/wp-content/uploads/2014/03/portrait-photography.jpg"),
+                    ),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(top: 4.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Expanded(
+                                  child: Container(child: RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(text:" Sean Kernan", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0, color: Colors.white),),
+                                      ]
+                                    ),overflow: TextOverflow.ellipsis,
+                                  )),flex: 5,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 4.0),
+                            child: Text("Asked at 25, September 2020", style: TextStyle(fontSize: 14.0, color: Colors.grey), maxLines: 1, ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              SizedBox(height: 10,),
+              Align(
+              alignment: Alignment.bottomLeft,
+              child: Text("What is the most fact about people you know", style: GoogleFonts.roboto(textStyle: TextStyle(color: Colors.white, fontSize: 22.5, fontWeight: FontWeight.bold)),),
+              ),
+              SizedBox(height: 25,),
+              Row( // the comments and likes
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  RichText(
+                text: TextSpan(
+                style: TextStyle(color: Colors.grey),
+                children: [
+                //TextSpan(text: 'Created with '),
+                WidgetSpan(
+                child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                child: Icon(Icons.thumb_up),
+              ),
+            ),
+            TextSpan(text: '  8', style: TextStyle(color: Colors.black, fontSize: 20)),
+          ],
+        ),
+      ),
+      SizedBox(width: 17.5),
+      RichText(
+                text: TextSpan(
+                style: TextStyle(color: Colors.grey),
+                children: [
+                //TextSpan(text: 'Created with '),
+                WidgetSpan(
+                child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                child: Icon(Icons.chat),
+              ),
+            ),
+            TextSpan(text: '  2', style: TextStyle(color: Colors.black, fontSize: 20)),
+          ],
+        ),
+      )
+                ],
+              ),
+              
+            
             ],
           ),
           
